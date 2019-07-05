5b99b19d3e88f38b13e6cdc22788fc909beaf3b2
yuanlixiao <yuanlixiao@live.cn>
2019-07-05 23:17:49 +0800

    chore:update

--- App.js
+++ App.js
@@ -6,9 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import { Image, StyleSheet, Text, View } from "react-native";
-
+import React, { Component } from "react";
+import { Image, FlatList, StyleSheet, Text, View } from "react-native";
 
 var REQUEST_URL =
   "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
@@ -43,16 +42,21 @@ var styles = StyleSheet.create({
   thumbnail: {
     width: 53,
     height: 81
-  }
+  },
+  list: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
 });
 
 type Props = {};
 // export default class App extends Component<Props> {
 class App extends Component<Props> {
   constructor(props) {
-    super(props);   //这一句不能省略，照抄即可
+    super(props);
     this.state = {
-      movies: null,  //这里放你自己定义的state变量及初始值
+      data: [],
+      loaded: false,
     };
     // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向不对
     // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
@@ -61,44 +65,54 @@ class App extends Component<Props> {
   componentDidMount() {
     this.fetchData();
   }
-  fetchData() {
+fetchData() {
     fetch(REQUEST_URL)
       .then((response) => response.json())
       .then((responseData) => {
         // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
         this.setState({
-          movies: responseData.movies,
+          data: this.state.data.concat(responseData.movies),
+          loaded: true,
         });
       });
+}
+render() {
+  if (!this.state.loaded) {
+    return this.renderLoadingView();
   }
-  render() {
-    if (!this.state.movies) {
-      return this.renderLoadingView();
-    }
-    var movie = this.state.movies[0];
-    return this.renderMovie(movie);
-  }
-  renderLoadingView() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          正在加载电影数据……
-        </Text>
-      </View>
+
+  return (
+    <FlatList
+      data={this.state.data}
+      renderItem={this.renderMovie}
+      style={styles.list}
+      keyExtractor={item => item.id}
+    />
+  );
+}
+renderLoadingView() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        正在加载电影数据……
+      </Text>
+    </View>
     );
   }
-  renderMovie(movie) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail}
-        />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year}>{movie.year}</Text>
+  renderMovie({ item }) {
+      // { item }是一种“解构”写法，请阅读ES2015语法的相关文档
+      // item也是FlatList中固定的参数名，请阅读FlatList的相关文档
+      return (
+        <View style={styles.container}>
+          <Image
+            source={{uri: item.posters.thumbnail}}
+            style={styles.thumbnail}
+          />
+          <View style={styles.rightContainer}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.year}>{item.year}</Text>
+          </View>
         </View>
-      </View>
     );
   }
 }
