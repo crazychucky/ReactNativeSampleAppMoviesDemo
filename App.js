518d229e19465c452512a7aba16c573d03aecfd1
yuanlixiao <yuanlixiao@live.cn>
2019-07-05 23:07:00 +0800

    chore:update

--- App.js
+++ App.js
@@ -9,6 +9,10 @@
 import React, {Component} from 'react';
 import { Image, StyleSheet, Text, View } from "react-native";
 
+
+var REQUEST_URL =
+  "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
+
 var MOCKED_MOVIES_DATA = [
   {
     title: "标题",
@@ -43,17 +47,60 @@ var styles = StyleSheet.create({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+// export default class App extends Component<Props> {
+class App extends Component<Props> {
+  constructor(props) {
+    super(props);   //这一句不能省略，照抄即可
+    this.state = {
+      movies: null,  //这里放你自己定义的state变量及初始值
+    };
+    // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向不对
+    // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
+    this.fetchData = this.fetchData.bind(this);
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
+        this.setState({
+          movies: responseData.movies,
+        });
+      });
+  }
   render() {
-  var movie = MOCKED_MOVIES_DATA[0];
-  return (
-    <View style={styles.container}>
-      <Image source={{ uri: movie.posters.thumbnail }} style={styles.thumbnail} />
-      <View style={styles.rightContainer}>
-        <Text style={styles.title}>{movie.title}</Text>
-        <Text style={styles.year}>{movie.year}</Text>
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  }
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          正在加载电影数据……
+        </Text>
       </View>
-    </View>
-  );
+    );
+  }
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
   }
 }
+
+export default App;
